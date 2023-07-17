3052724332f77883888e4893ba3c385d8d0c4af3
Zafar37 <goliburakov535@gmail.com>
2023-07-17 04:28:50 -0700

    Finished Project Doviz

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine(@"Want you consvert dollar to gold?
+    Then you are at the right place!");
+
+Console.Write("Enter dollar amount: ");
+var money = double.Parse(Console.ReadLine()!);
+
+double convertToGold = money * 0.016;
+
+Console.WriteLine("\tConversion is in progress...");
+Console.WriteLine($"Gold: {convertToGold} gramm");
+
+Console.WriteLine("\nEnetr the word \"dotnet run\" to convert again.");
