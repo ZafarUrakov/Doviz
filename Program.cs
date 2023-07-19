64c1e6234a4035181bdc4d1cbb1c2fac06de05a7
Zafar37 <goliburakov535@gmail.com>
2023-07-19 03:44:19 -0700

    Updates Data Types

--- Program.cs
+++ Program.cs
@@ -2,11 +2,11 @@
     Then you are at the right place!");
 
 Console.Write("Enter dollar amount: ");
-var money = double.Parse(Console.ReadLine()!);
+double money = double.Parse(Console.ReadLine()!);
 
 double convertToGold = money * 0.016;
 
 Console.WriteLine("\tConversion is in progress...");
 Console.WriteLine($"Gold: {convertToGold} gramm");
 
-Console.WriteLine("\nEnetr the word \"dotnet run\" to convert again.");
+Console.WriteLine("\nEnter the word \"dotnet run\" to convert again.");
